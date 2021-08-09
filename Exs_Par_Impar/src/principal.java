5fabdcb1487d1c29b0a5c766c0dd54a4e9de0034
Leonardo Ribeiro Leonardi <49618344+Leo99z@users.noreply.github.com>
2021-08-09 09:27:57 -0300

    Exs_Par_Ou_Impar

---
+++ Exs_Par_Impar/src/principal.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class principal {
+
+    public static void main(String[] args) {
+        String saida = "";
+        String entrada = JOptionPane.showInputDialog("Digite um número:");
+        int i = Integer.parseInt(entrada);
+        if(i % 2 == 0){
+            saida += "Número é PAR";
+        }
+        else{
+            saida += "Número é IMPAR";
+        }
+        JOptionPane.showMessageDialog(null,"Valor: "+saida,"Testa numero",JOptionPane.INFORMATION_MESSAGE);
+        System.exit(0);
+    }
+}
