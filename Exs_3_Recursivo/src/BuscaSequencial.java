8c58f55ad7f3cd53250c94bb8fc089ebf22baa80
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2021-09-22 12:26:42 -0300

    Correção de bug

--- Exs_3_Recursivo/src/BuscaSequencial.java
+++ Exs_3_Recursivo/src/BuscaSequencial.java
@@ -6,10 +6,10 @@ public class BuscaSequencial {
     }
 
     private static int buscaSequencial(int[] vetor, int elemento, int i) {
-        if (elemento != vetor[i]) {
-            return buscaSequencial(vetor, elemento, i + 1);
-        }else if(i > vetor.length - 1){
+        if (i > vetor.length - 1) {
             return -1;
+        }else if(elemento != vetor[i]){
+            return buscaSequencial(vetor, elemento, i + 1);
         }else{
             return i;
         }
